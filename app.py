3adda73beb89d178dd10ed3f229a870f09f57e06
Jackson R B Alfonso <jackson.alfonso@gmail.com>
2022-12-21 23:54:25 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -4,13 +4,21 @@ import altair as alt
 import plotly.express as px
 
 @st.cache
-def busca_dados():
+def busca_dados(uploaded_file):
     # importando os dados
-    df = pd.read_excel('controle.xlsx', index_col=0)
+    if uploaded_file:
+        origem = uploaded_file
+    else:
+        origem = 'controle.xlsx'
+
+    df = pd.read_excel(origem, index_col=0)
+    
+    if not {'NOME', 'DATA', 'PAGO'}.issubset(df.columns):
+        return pd.DataFrame()
+
     df['Status'] = df.PAGO.map({1:"Pago", 0:"Pendente"})
     df['Valor'] = 816 / len(df)
-    colunas = ["NOME","Status","Valor"]
-    return df[colunas].copy().reset_index()
+    return df.reset_index()
 
 def cria_barras():
     bars = alt.Chart(dados).mark_bar().encode(
@@ -34,74 +42,85 @@ st.set_page_config(page_title="Controle Assinatura",
                 layout="wide"
                 )
 
-colunas = ["NOME","Status","Valor"]
-dados = busca_dados()[colunas]
+with st.expander("Upload Planilha"):
+    uploaded_file = st.file_uploader("Fazer Upload da planilha de controle", type='xlsx')
 
-# filtros para a tabela
-st.sidebar.markdown('## Filtro para a tabela')
+#if  uploaded_file:
+dados = busca_dados(uploaded_file)
+#else:
+#    dados = pd.DataFrame()
+st.title('Controle Assinaturas :black_nib:  - SELECIONE UM ARQUIVO PARA VISUALIZAR')
 
-st.sidebar.header("Selecione uma opção aqui:")
-status = st.sidebar.multiselect(
-    "Selecione o Status desejado:",
-    options=dados['Status'].unique(),
-    default=dados['Status'].unique()
-)
+if len(dados) > 0:
+    # filtros para a tabela
+    st.sidebar.markdown('## Filtro para a tabela')
 
-st.title('Controle Assinaturas :black_nib:')
-st.write('Nesse projeto vamos analisar os pagamentos realizados de quem aderiu ao plano de assinatura para estudos.')
-st.write(':zap::cool: Desenvolvido com :blue[Streamlit] e :red[Python] :heavy_check_mark::sunglasses::smile:')
-st.markdown("##")
+    st.sidebar.header("Selecione uma opção aqui:")
+    status = st.sidebar.multiselect(
+        "Selecione o Status desejado:",
+        options=dados['Status'].unique(),
+        default=dados['Status'].unique()
+    )
 
-total_pago = int(dados[dados.Status == "Pago"].Valor.sum())
-total_pendente = int(dados[dados.Status == "Pendente"].Valor.sum())
-total_assinatura = int(dados.Valor.sum())
+    st.title('Controle Assinaturas :black_nib:')
+    st.write('Nesse projeto vamos analisar os pagamentos realizados de quem aderiu ao plano de assinatura para estudos.')
+    st.write(':zap::cool: Desenvolvido com :blue[Streamlit] e :red[Python] :heavy_check_mark::sunglasses::smile:')
+    st.markdown("##")
 
-esquerda, centro, direita = st.columns(3)
-
-with esquerda:
-    st.subheader("Assinatura Total")
-    st.subheader(f"R$ {total_assinatura:,}")
-with centro:
-    st.subheader("Total Pago")
-    st.subheader(f"R$ {total_pago:,}")
-with direita:
-    st.subheader("Total Pendente")
-    st.subheader(f"R$ {total_pendente:,}")
-st.markdown("---")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.header("Detalhamento")
-    df_situacao = dados.query('Status == @status')
+    total_pago = int(dados[dados.Status == "Pago"].Valor.sum())
+    qtde_pago = int(dados[dados.Status == "Pago"].NOME.count())
     
-    st.table(df_situacao[colunas])
-
-with col2:
-    st.header("Resumo da Situação")
-    df_grouped = dados.groupby("Status")["Valor"].sum()
-
-    #cria_barras()    
+    total_pendente = int(dados[dados.Status == "Pendente"].Valor.sum())
+    qtde_pendente = int(dados[dados.Status == "Pendente"].NOME.count())
     
-    pagamento_por_situacao = (
-            dados.groupby("Status").count()[['Valor']].sort_values(by="Status", ascending=False)
-    )
+    total_assinatura = int(dados.Valor.sum())
+    total_assinantes = int(dados.NOME.count())
 
-    fig_status = px.bar(
-        pagamento_por_situacao,
-        x="Valor",
-        y=pagamento_por_situacao.index,
-        orientation="h",
-        title="<b>Status Pagamento</b>",
-        color_discrete_sequence=["#0083B8"] * len(pagamento_por_situacao),
-        #color='Valor',
-        text_auto='.1s',
-        template='plotly_white',
-    )
-    fig_status.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
-    fig_status.update_layout(
-        plot_bgcolor="rgba(0,0,0,0)",
-        xaxis=(dict(showgrid=False))
-    )
+    esquerda, centro, direita = st.columns(3)
 
-    st.plotly_chart(fig_status)
+    with esquerda:
+        st.subheader("Valor Assinatura")
+        st.subheader(f"R$ {total_assinatura:,} \n (Qtde Assinantes: {total_assinantes}) ")
+    with centro:
+        st.subheader("Total Pago")
+        st.subheader(f"R$ {total_pago:,} \n (Qtde Pagantes: {qtde_pago})")
+    with direita:
+        st.subheader("Total Pendente")
+        st.subheader(f"R$ {total_pendente:,} \n (Qtde Pendente: {qtde_pendente})")
+    st.markdown("---")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.header("Detalhamento")
+        df_situacao = dados.query('Status == @status')
+        colunas = ['NOME', 'Status', 'Valor']
+        st.table(df_situacao[colunas])
+
+    with col2:
+        st.header("Resumo da Situação")
+        df_grouped = dados.groupby("Status")["Valor"].sum()
+
+        #cria_barras()    
+        
+        pagamento_por_situacao = (
+                dados.groupby("Status").count()[['Valor']].sort_values(by="Status", ascending=False)
+        )
+
+        fig_status = px.bar(
+            pagamento_por_situacao,
+            x="Valor",
+            y=pagamento_por_situacao.index,
+            orientation="h",
+            title="<b>Status Pagamento</b>",
+            color_discrete_sequence=["#0083B8"] * len(pagamento_por_situacao),
+            #color='Valor',
+            template='plotly_white',
+        )
+        fig_status.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+        fig_status.update_layout(
+            plot_bgcolor="rgba(0,0,0,0)",
+            xaxis=(dict(showgrid=False))
+        )
+
+        st.plotly_chart(fig_status)
